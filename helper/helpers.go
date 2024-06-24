c6f9793df0740ea32f00a6d470bb8a5d14a1aa44
nohgo <nohgoering@gmail.com>
2024-06-24 18:53:00 -0400

    add untested token creation and parsing

--- helper/helpers.go
+++
@@ -1,9 +0,0 @@
-package help
-
-func StrToByteSlice(str string) (result []byte) {
-	result = make([]byte, len(str))
-	for i, v := range str {
-		result[i] = byte(v)
-	}
-	return
-}
---
+++ security/jwt.go
@@ -0,0 +1,33 @@
+package security
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func CreateToken() (tokenString string, err error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"name": "noh",
+	})
+	tokenString, err = token.SignedString("hello")
+	return
+}
+
+func ParseToken(tokenString string) (string, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return "hello", nil
+	})
+	if err != nil {
+		return "", err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok {
+		return "", errors.New("invalid token")
+	}
+	switch claims["name"].(type) {
+	case string:
+		return claims["name"].(string), err
+	default:
+		return "", errors.New("invalid claims")
+	}
+}
