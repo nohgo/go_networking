78d217865b1f0fbf5cbcbdb65c0dc1bd9134ab8d
nohgo <nohgoering@gmail.com>
2024-06-30 19:15:21 -0400

    add database connection and repository layer

--- api/database/database.go
+++ api/database/database.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -10,12 +11,17 @@ import (
 var Pool *sql.DB
 
 func CreatePool() {
-	tempDB, err := sql.Open("postgres", "connString")
+	tempDB, err := sql.Open("postgres", os.Getenv("GO_CONN_STRING"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	Pool = tempDB
+
+	err = Pool.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func ClosePool() {
--- api/database/repositories/userRepository.go
+++ api/database/repositories/userRepository.go
@@ -1,13 +1,31 @@
-package db
+package repo
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
 	"github.com/nohgo/go_networking/api/database"
-	"log"
+	"github.com/nohgo/go_networking/api/models"
 )
 
-var pool = db.Pool
+type UserRepository interface {
+	Add(models.User) error
+	GetAll() ([]models.User, error)
+}
 
-func getAll() {
+type postgresUserRepository struct {
+	pool *sql.DB
+}
+
+func NewUserRepository() *postgresUserRepository {
+	return &postgresUserRepository{db.Pool}
+}
+
+func (ur *postgresUserRepository) Add(user models.User) error {
+	_, err := ur.pool.Exec(fmt.Sprintf("INSERT INTO users (username, password) VALUES ('%v', '%v')", user.Username, user.Password))
+	return err
+}
+
+func (*postgresUserRepository) GetAll() ([]models.User, error) {
+	return nil, nil
 }
