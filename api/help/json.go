8292c46a22608072d47a09dfd613b79041640451
nohgo <nohgoering@gmail.com>
2024-07-07 18:38:44 -0400

    add proper error handling to json decoding

--- api/help/json.go
+++ api/help/json.go
@@ -2,17 +2,40 @@ package help
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
+	"strings"
 )
 
 func DecodeStruct[F any](r *http.Request, dest *F) (error, int) {
 	err := json.NewDecoder(r.Body).Decode(dest)
 
-	if err != nil {
-		return err, 500
+	if err == nil {
+		return nil, 200
 	}
 
-	return nil, 200
+	var syntaxError *json.SyntaxError
+	var unmarshallTypeError *json.UnmarshalTypeError
+
+	var msg string
+
+	switch {
+	case errors.As(err, &syntaxError), errors.Is(err, io.ErrUnexpectedEOF):
+		msg = "Request body contains malformed json"
+	case strings.HasPrefix(err.Error(), "json: unknown field "):
+		msg = "Unknown field in json"
+	case errors.As(err, &unmarshallTypeError):
+		msg = "Request body contains an invalid type"
+	case errors.Is(err, io.EOF):
+		msg = "Request body missing"
+	default:
+		msg = err.Error()
+		return errors.New(msg), http.StatusInternalServerError
+	}
+
+	return errors.New(msg), http.StatusBadRequest
+
 }
 
 func SendJson(w http.ResponseWriter, target any) {
