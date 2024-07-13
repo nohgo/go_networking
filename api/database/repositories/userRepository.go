c6ab0e994db6937a38776a36f5d4021e752a23e1
nohgo <nohgoering@gmail.com>
2024-07-12 20:24:21 -0400

    add testing to repository layer

--- api/database/repositories/userRepository.go
+++ api/database/repositories/userRepository.go
@@ -12,6 +12,7 @@ import (
 type UserRepository interface {
 	Add(models.User) error
 	AreValidCredentials(models.User) (bool, error)
+	Delete(user models.User) error
 }
 
 type postgresUserRepository struct {
@@ -45,4 +46,9 @@ func (ur *postgresUserRepository) AreValidCredentials(user models.User) (bool, e
 	}
 
 	return true, nil
+}
+
+func (ur *postgresUserRepository) Delete(user models.User) error {
+	_, err := ur.pool.Exec("DELETE FROM users WHERE username=$1", user.Username)
+	return err
 }
