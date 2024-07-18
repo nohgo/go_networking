5ef9c93b54ee17766151325f71a0f84736474b57
nohgo <nohgoering@gmail.com>
2024-07-17 20:27:47 -0400

    add user service testing

--- api/services/tests/UserService_test.go
+++ api/services/tests/UserService_test.go
@@ -12,12 +12,29 @@ import (
 var user models.User
 
 func TestMain(m *testing.M) {
-	mur := repo_mock.NewMockUserRepository()
-	us := svc.NewUserService(mur)
 	user = models.User{
 		Username: "bob",
 		Password: "hello",
 	}
 	code := m.Run()
 	os.Exit(code)
+}
+
+func TestRegister(t *testing.T) {
+	us := svc.NewUserService(repo_mock.NewMockUserRepository())
+	if err := us.Register(user); err != nil {
+		t.Fatalf("Register failed: %v", err)
+	}
+}
+
+func TestLogin(t *testing.T) {
+	us := svc.NewUserService(repo_mock.NewMockUserRepository())
+	token, err := us.Login(user)
+
+	if err != nil {
+		t.Fatalf("Login returned an error: %v", err)
+	}
+	if token == "" {
+		t.Fatalf("Login returned a blank token: %v", token)
+	}
 }
